Garux/netradiant-custom

commit 9a13f9ed552ec7a7f056557ce922746df22b970f

simplify build code

radiant/qe3.cpp
@@ -37,7 +37,6 @@
 #include "debugging/debugging.h"
 
 #include "ifilesystem.h"
-//#include "imap.h"
 
 #include <map>
 
@@ -59,14 +58,9 @@
 #include "preferences.h"
 #include "watchbsp.h"
 #include "autosave.h"
-#include "convert.h"
 
 QEGlobals_t g_qeglobals;
 
-
-#if defined( WIN32 )
-#define PATH_MAX 260
-#endif
 
 #if defined( POSIX )
 #include <sys/stat.h> // chmod
@@ -193,27 +187,7 @@ void bsp_shutdown(){
 	build_clear_variables();
 }
 
-class ArrayCommandListener : public CommandListener
-{
-	GPtrArray* m_array;
-public:
-	ArrayCommandListener(){
-		m_array = g_ptr_array_new();
-	}
-	~ArrayCommandListener(){
-		g_ptr_array_free( m_array, TRUE );
-	}
-
-	void execute( const char* command ){
-		g_ptr_array_add( m_array, g_strdup( command ) );
-	}
-
-	GPtrArray* array() const {
-		return m_array;
-	}
-};
-
-class BatchCommandListener : public CommandListener
+class BatchCommandListener
 {
 	TextOutputStream& m_file;
 	std::size_t m_commandCount;
@@ -255,32 +229,25 @@ void RunBSP( size_t buildIdx ){
 
 	bsp_init();
 
-	ArrayCommandListener listener;
-	build_run( buildIdx, listener );
-	bool monitor = false;
-	for ( guint i = 0; i < listener.array()->len; ++i )
-		if( strstr( (char*)g_ptr_array_index( listener.array(), i ), RADIANT_MONITOR_ADDRESS ) )
-			monitor = true;
+	const std::vector<CopiedString> commands = build_construct_commands( buildIdx );
+	const bool monitor = std::any_of( commands.cbegin(), commands.cend(), []( const CopiedString& command ){
+		return strstr( command.c_str(), RADIANT_MONITOR_ADDRESS ) != 0;
+	} );
 
 	if ( g_WatchBSP_Enabled && monitor ) {
 		// grab the file name for engine running
 		const char* fullname = Map_Name( g_map );
 		const auto bspname = StringOutputStream( 64 )( PathFilename( fullname ) );
-		BuildMonitor_Run( listener.array(), bspname.c_str() );
+		BuildMonitor_Run( commands, bspname.c_str() );
 	}
 	else
 	{
-		char junkpath[PATH_MAX];
-		strcpy( junkpath, SettingsPath_get() );
-		strcat( junkpath, "junk.txt" );
+		const auto junkpath = StringOutputStream( 256 )( SettingsPath_get(), "junk.txt" );
 
-		char batpath[PATH_MAX];
 #if defined( POSIX )
-		strcpy( batpath, SettingsPath_get() );
-		strcat( batpath, "qe3bsp.sh" );
+		const auto batpath = StringOutputStream( 256 )( SettingsPath_get(), "qe3bsp.sh" );
 #elif defined( WIN32 )
-		strcpy( batpath, SettingsPath_get() );
-		strcat( batpath, "qe3bsp.bat" );
+		const auto batpath = StringOutputStream( 256 )( SettingsPath_get(), "qe3bsp.bat" );
 #else
 #error "unsupported platform"
 #endif
@@ -292,7 +259,8 @@ void RunBSP( size_t buildIdx ){
 				batchFile << "#!/bin/sh \n\n";
 #endif
 				BatchCommandListener listener( batchFile, g_WatchBSP0_DumpLog? junkpath : 0 );
-				build_run( buildIdx, listener );
+				for( const auto& command : commands )
+					listener.execute( command.c_str() );
 				written = true;
 			}
 		}

contrib/prtview/LoadPortalFileDialog.cpp
@@ -23,7 +23,6 @@
 #include "LoadPortalFileDialog.h"
 
 #include "stream/stringstream.h"
-#include "convert.h"
 #include "os/path.h"
 
 #include "qerplugin.h"

libs/xml/xmlparser.h
@@ -25,7 +25,7 @@
 #include <cstring>
 #include "ixml.h"
 #include "libxml/parser.h"
-#include "convert.h"
+#include "stream/textstream.h"
 
 class TextInputStream;
 

libs/xml/xmlwriter.h
@@ -21,9 +21,9 @@
 
 #pragma once
 
-#include "convert.h"
 #include <vector>
 #include "xml/ixml.h"
+#include "stream/textstream.h"
 
 class XMLEntityOutputStream
 {

radiant/build.cpp
@@ -468,13 +468,15 @@ void project_verify( Project& project, Tools& tools ){
 #endif
 }
 
-void build_run( size_t buildIdx, CommandListener& listener ){
+std::vector<CopiedString> build_construct_commands( size_t buildIdx ){
 	for ( const auto& [ name, tool ] : g_build_tools )
 	{
 		StringBuffer output;
 		tool.evaluate( output );
 		build_set_variable( name.c_str(), output.c_str() );
 	}
+
+	std::vector<CopiedString> commands;
 
 	if ( const auto buildIt = Project_find( g_build_project, buildIdx ); buildIt != g_build_project.end() )
 	{
@@ -484,13 +486,15 @@ void build_run( size_t buildIdx, CommandListener& listener ){
 			StringBuffer output;
 			command.evaluate( output );
 			if ( !output.empty() )
-				listener.execute( output.c_str() );
+				commands.emplace_back( output.c_str() );
 		}
 	}
 	else
 	{
 		globalErrorStream() << "build #" << buildIdx << " not defined";
 	}
+
+	return commands;
 }
 
 

radiant/build.h
@@ -21,16 +21,13 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
+#include "string/string.h"
 
 void build_set_variable( const char* name, const char* value );
 void build_clear_variables();
 
-class CommandListener
-{
-public:
-	virtual void execute( const char* command ) = 0;
-};
-void build_run( size_t buildIdx, CommandListener& listener );
+std::vector<CopiedString> build_construct_commands( size_t buildIdx );
 
 void DoBuildMenu();
 

radiant/dialog.cpp
@@ -36,7 +36,6 @@
 #include <cstdlib>
 
 #include "stream/stringstream.h"
-#include "convert.h"
 #include "gtkutil/dialog.h"
 #include "gtkutil/entry.h"
 #include "gtkutil/image.h"

radiant/entityinspector.cpp
@@ -60,7 +60,6 @@
 #include "os/file.h"
 #include "stream/stringstream.h"
 #include "moduleobserver.h"
-#include "convert.h"
 #include "stringio.h"
 
 #include "gtkutil/accelerator.h"

F1F7Y/MRVN-radiant

commit f9471b8676fefec9defbc10f981a481535c3ae3f

fix nonmonitored builds broken in 9a13f9ed552ec7a7f056557ce922746df22b970f

libs/stream/stringstream.h
@@ -104,7 +104,7 @@ public:
 
 	StringOutputStream(){
 	}
-	StringOutputStream( std::size_t capacity ) : m_string( capacity ){
+	explicit StringOutputStream( std::size_t capacity ) : m_string( capacity ){
 	}
 	std::size_t write( const char* buffer, std::size_t length ){
 		m_string.push_range( buffer, buffer + length );

radiant/qe3.cpp
@@ -256,7 +256,7 @@ void RunBSP( size_t buildIdx ){
 #if defined ( POSIX )
 				batchFile << "#!/bin/sh \n\n";
 #endif
-				BatchCommandListener listener( batchFile, g_WatchBSP0_DumpLog? junkpath : 0 );
+				BatchCommandListener listener( batchFile, g_WatchBSP0_DumpLog? junkpath.c_str() : nullptr );
 				for( const auto& command : commands )
 					listener.execute( command.c_str() );
 				written = true;